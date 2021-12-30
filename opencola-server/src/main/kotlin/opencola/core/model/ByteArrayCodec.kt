40b9da91a927589b61803348882e817594d4ee69
John Midgley <john.midgley@gmail.com>
2021-12-29 16:55:14 -0800

    Added long codec

--- opencola-server/src/main/kotlin/opencola/core/model/ByteArrayCodec.kt
+++ opencola-server/src/main/kotlin/opencola/core/model/ByteArrayCodec.kt
@@ -1,6 +1,21 @@
 package opencola.core.model
 
+import opencola.core.extensions.nullOrElse
+import java.nio.ByteBuffer
+
+// TODO: Can this be generified?
 interface ByteArrayCodec {
     fun encode(value: Any?): ByteArray
     fun decode(value: ByteArray?): Any?
+}
+
+// TODO - Move codecs from delegates here and add codec property to delegates that point to objects here
+object LongByteArrayCodec : ByteArrayCodec {
+    override fun encode(value: Any?): ByteArray {
+        return ByteBuffer.allocate(Long.SIZE_BYTES).putLong(value as Long).array()
+    }
+
+    override fun decode(value: ByteArray?): Long? {
+        return value.nullOrElse { ByteBuffer.wrap(it).long }
+    }
 }
