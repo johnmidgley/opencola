78a4c7195cd9f627df8615c5bbdce95e9a37a72e
John Midgley <john.midgley@gmail.com>
2022-01-01 00:45:59 -0800

    - Fixed warnings

--- opencola-server/src/main/kotlin/opencola/server/plugins/HTTP.kt
+++ opencola-server/src/main/kotlin/opencola/server/plugins/HTTP.kt
@@ -3,8 +3,6 @@ package opencola.server.plugins
 import io.ktor.http.*
 import io.ktor.features.*
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 
 fun Application.configureHTTP() {
     install(CORS) {
