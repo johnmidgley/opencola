b7dacfa15ebfbb5c2ec3535058673b6c6e068c0a
John Midgley <john.midgley@gmail.com>
2022-01-02 19:55:40 -0800

    Cleaned up mhtml test.

--- opencola-server/src/test/kotlin/opencola/core/MhtmlParserTest.kt
+++ opencola-server/src/test/kotlin/opencola/core/MhtmlParserTest.kt
@@ -2,24 +2,22 @@ package opencola.core
 
 import opencola.core.content.MhtmlPage
 import opencola.core.content.parseMime
+import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.inputStream
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class MhtmlParserTest {
+    private fun readMhtmlPage(path: Path): MhtmlPage {
+        return MhtmlPage(path.inputStream().use { parseMime(it) ?: throw RuntimeException("Unable to parse $it") })
+    }
+
     @Test
     fun testContentEquals(){
         val rootPath = Path(System.getProperty("user.dir"),"..", "sample-docs").toString()
-
-        val path1 = Path(rootPath, "Conway's Game of Life - Wikipedia.mht")
-        val message1 = path1.inputStream().use { parseMime(it) } ?: throw RuntimeException("Unable to parse $path1")
-        val mhtmlPage1 = MhtmlPage(message1)
-
-        val path2 = Path(rootPath, "Conway's Game of Life - Wikipedia 2.mht")
-        val message2 = path2.inputStream().use { parseMime(it) } ?: throw RuntimeException("Unable to parse $path2")
-        val mhtmlPage2 = MhtmlPage(message2)
-
+        val mhtmlPage1 = readMhtmlPage(Path(rootPath, "Conway's Game of Life - Wikipedia.mht"))
+        val mhtmlPage2 = readMhtmlPage(Path(rootPath, "Conway's Game of Life - Wikipedia 2.mht"))
         assertEquals(mhtmlPage1.getDataId(), mhtmlPage2.getDataId())
     }
 }
