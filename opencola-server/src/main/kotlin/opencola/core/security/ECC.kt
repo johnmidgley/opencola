4018411d7f391191b5d72275cd3b3d19f8c65a41
John Midgley <john.midgley@gmail.com>
2022-01-26 09:16:40 -0800

    Certificate work

--- opencola-server/src/main/kotlin/opencola/core/security/ECC.kt
+++ opencola-server/src/main/kotlin/opencola/core/security/ECC.kt
@@ -1,6 +1,18 @@
 package opencola.core.security
 
+import opencola.core.extensions.toByteArray
+import opencola.core.model.Id
+import org.bouncycastle.asn1.x500.X500NameBuilder
+import org.bouncycastle.asn1.x500.style.BCStyle
+import org.bouncycastle.cert.X509CertificateHolder
+import org.bouncycastle.cert.X509v1CertificateBuilder
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
+import org.bouncycastle.cert.jcajce.JcaX509v1CertificateBuilder
+import org.bouncycastle.operator.ContentSigner
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder
+import java.math.BigInteger
 import java.security.*
+import java.security.cert.X509Certificate
 import java.security.spec.ECGenParameterSpec
 import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
@@ -46,4 +58,44 @@ fun publicKeyFromBytes(bytes: ByteArray) : PublicKey {
 
 fun privateKeyFromBytes(bytes: ByteArray) : PrivateKey {
     return getKeyFactory().generatePrivate(PKCS8EncodedKeySpec(bytes))
+}
+
+fun calculateDate(hoursInFuture: Int) : Date {
+    val secs = System.currentTimeMillis() / 1000;
+    return Date((secs + (hoursInFuture * 60 * 60)) * 1000);
+}
+
+fun createAuthority(){
+    // Need to store full credential (i.e. including private key - private key is not in cert)
+    val id = Id.ofData(UUID.randomUUID().toByteArray())
+    val keyPair = generateKeyPair()
+    val cert = createCertificate(id, keyPair)
+
+
+}
+
+
+// This creates a V1 certificate. Would there ever be a case that a CA is needed? Would someone want to grant the
+// ability to a trusted party to create certificates on their behalf?
+// A history of certificates will be needed in order to validate historical transactions.
+// Is it sufficient to replace a certificate (i.e. each user has only 1 active cert) vs. managing CRLs?
+fun createCertificate(id: Id, keyPair: KeyPair): X509Certificate {
+    val x500NameBld = X500NameBuilder(BCStyle.INSTANCE).addRDN(BCStyle.O, "openCOLA").addRDN(BCStyle.CN, "opencola:$id")
+
+    val name = x500NameBld.build()
+
+    // This is a v1 certificate - doesn't support extensions
+    val certBuilder: X509v1CertificateBuilder = JcaX509v1CertificateBuilder(
+        name,
+        BigInteger.valueOf(0), // calculateSerialNumber(),
+        calculateDate(0),
+        calculateDate(24 * 31),
+        name,
+        keyPair.public
+    )
+
+    val signer: ContentSigner = JcaContentSignerBuilder(SIGNATURE_ALGO).setProvider("BC").build(keyPair.private)
+
+    return JcaX509CertificateConverter().setProvider("BC")
+        .getCertificate(certBuilder.build(signer))
 }
