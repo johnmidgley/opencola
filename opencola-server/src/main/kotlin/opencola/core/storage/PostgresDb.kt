8637caeeb159a40078f15e8cf2bdfbebe213b7ed
John Midgley <john.midgley@gmail.com>
2022-01-26 19:25:41 -0800

    Switched from "opencola" to "postgres" since creating a postgres DB automatically does not seem trivial

--- opencola-server/src/main/kotlin/opencola/core/storage/PostgresDb.kt
+++ opencola-server/src/main/kotlin/opencola/core/storage/PostgresDb.kt
@@ -5,7 +5,8 @@ import org.jetbrains.exposed.sql.Database
 object PostgresDb {
     val db by lazy {
         // TODO: Consider https://github.com/pgjdbc/pgjdbc from https://github.com/JetBrains/Exposed/wiki/DataBase-and-DataSource
-        Database.connect("jdbc:postgresql://localhost:5432/opencola", driver = "org.postgresql.Driver",
+        // TODO: Using postgres db, as default. Should create opencola
+        Database.connect("jdbc:postgresql://localhost:5432/postgres", driver = "org.postgresql.Driver",
             user = "postgres", password = "example")
     }
 }
