c6f9345978b846083e2511a3afa3f327d918553d
John Midgley <john.midgley@gmail.com>
2022-02-09 13:35:15 -0800

    Throw error if no query specified.

--- opencola-server/src/main/kotlin/opencola/server/SearchHandler.kt
+++ opencola-server/src/main/kotlin/opencola/server/SearchHandler.kt
@@ -6,7 +6,7 @@ import opencola.server.config.App
 
 class SearchHandler(private val call: ApplicationCall) {
     suspend fun respond(){
-        val query = call.request.queryParameters["q"] ?: return call.respond(SearchResults("", emptyList()))
+        val query = call.request.queryParameters["q"] ?: throw IllegalArgumentException("No query (q) specified in parameters")
         val results = App.searchService.search(query).map { SearchResult(it.entityId.toString(), it.name, it.description) }
         call.respond(SearchResults(query, results))
     }
