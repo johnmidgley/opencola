cfbe9a23a332e4d9d640c2496088ad40726ba195
John Midgley <john.midgley@gmail.com>
2022-02-10 12:19:58 -0800

    Comment

--- opencola-server/src/main/kotlin/opencola/core/security/Signator.kt
+++ opencola-server/src/main/kotlin/opencola/core/security/Signator.kt
@@ -4,6 +4,7 @@ import opencola.core.model.Id
 
 class Signator(private val keystore: KeyStore) {
     fun signBytes(authorityId: Id, bytes: ByteArray): ByteArray {
+        // TODO: Sign bytes or hash of bytes? Check performance diff
         val privateKey = keystore.getPrivateKey(authorityId)
             ?: throw RuntimeException("Unable to find private key for Authority: $authorityId")
 
