225e8daca26a4041c04472bee1a4f4a4d0783669
John Midgley <john.midgley@gmail.com>
2022-02-17 16:56:37 -0800

    Cleaned up styling

--- extension/chrome/popup.js
+++ extension/chrome/popup.js
@@ -70,10 +70,4 @@ rate.addEventListener("click", async () => {
     alert("rate: " + tab.url)
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
-}
+
