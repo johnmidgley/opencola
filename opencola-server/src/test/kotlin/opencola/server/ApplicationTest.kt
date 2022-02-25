919cd6377f5680a7e30aad97ba8e979a74230149
John Midgley <john.midgley@gmail.com>
2022-02-24 16:49:46 -0800

    Properly initialized test application

--- opencola-server/src/test/kotlin/opencola/server/ApplicationTest.kt
+++ opencola-server/src/test/kotlin/opencola/server/ApplicationTest.kt
@@ -3,9 +3,14 @@ package opencola.server
 import io.ktor.http.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import opencola.core.TestApplication
 import opencola.server.plugins.configureRouting
 
 class ApplicationTest {
+    init{
+        TestApplication.init()
+    }
+
     @Test
     fun testRoot() {
         withTestApplication({ configureRouting() }) {
