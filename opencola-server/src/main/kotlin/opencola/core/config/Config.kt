36beb68b443915feecb21b54ffa120e4c7fd57cc
John Midgley <john.midgley@gmail.com>
2022-03-09 14:33:11 -0800

    Added peer configuration

--- opencola-server/src/main/kotlin/opencola/core/config/Config.kt
+++ opencola-server/src/main/kotlin/opencola/core/config/Config.kt
@@ -2,23 +2,16 @@ package opencola.core.config
 
 import com.sksamuel.hoplite.ConfigLoader
 import java.nio.file.Path
-import kotlin.io.path.createDirectory
-import kotlin.io.path.exists
 
 // TODO: Add config layers, that allow for override. Use push / pop
 // https://github.com/sksamuel/hoplite
 data class Server(val host: String, val port: Int)
-
 data class Filestore(val name: String = "filestore")
-
 data class Storage(val path: Path, val filestore: Filestore = Filestore())
-
 data class Keystore(val name: String, val password: String)
-
 data class Security(val keystore: Keystore)
-
-data class Network(val peers: List<String>)
-
+data class Peer(val id: String, val name: String, val ip: String)
+data class Network(val peers: List<Peer>)
 data class Config(val env: String,
                   val server: Server?,
                   val storage: Storage,
