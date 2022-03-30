61871a5fae8fe3e03a6d002567f979d6f6de78d6
John Midgley <john.midgley@gmail.com>
2022-03-30 13:15:44 -0700

    Added comment

--- opencola-server/src/main/kotlin/opencola/core/model/Actions.kt
+++ opencola-server/src/main/kotlin/opencola/core/model/Actions.kt
@@ -3,4 +3,5 @@ package opencola.core.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+// TODO - Break into individual actions?
 data class Actions(val save: Boolean? = null, val trust: Float? = null, val like: Boolean? = null, val rating: Float? = null)
