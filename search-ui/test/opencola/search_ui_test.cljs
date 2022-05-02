e5313d04a47640d054e154aa24f7e88f1eb3ec60
John Midgley <john.midgley@gmail.com>
2022-05-02 11:42:58 -0700

    Moved search-ui -> web-ui

--- search-ui/test/opencola/search_ui_test.cljs
+++
@@ -1,5 +0,0 @@
-(ns opencola.search-ui-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [opencola.search-ui]))
-
--- search-ui/test/opencola/test_runner.cljs
+++
@@ -1,9 +0,0 @@
-;; This test runner is intended to be run from the command line
-(ns opencola.test-runner
-  (:require
-    ;; require all the namespaces that you want to test
-    [opencola.search-ui-test]
-    [figwheel.main.testing :refer [run-tests-async]]))
-
-(defn -main [& args]
-  (run-tests-async 5000))
