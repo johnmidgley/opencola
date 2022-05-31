6715da748294454fbc3fd46be79c7e6bb0e3bf55
John Midgley <john.midgley@gmail.com>
2022-05-31 15:04:26 -0700

    - Added Base58 encoder/decoder

---
+++ opencola-server/src/test/kotlin/opencola/core/serialization/Base58Test.kt
@@ -0,0 +1,25 @@
+package opencola.core.serialization
+
+import opencola.core.content.Base58
+import opencola.core.model.Id
+import opencola.core.security.generateKeyPair
+import org.junit.Test
+import kotlin.test.assertContentEquals
+
+class Base58Test {
+    @Test
+    fun testEncodeIdBase58(){
+        val idBytes = Id.encode(Id.new())
+        val decodedBytes = Base58.decode(Base58.encode(idBytes))
+
+        assertContentEquals(idBytes, decodedBytes)
+    }
+
+    @Test
+    fun testEncodePublicKeyBase58(){
+        val publicKeyBytes = generateKeyPair().public.encoded
+        val decodedBytes = Base58.decode(Base58.encode(publicKeyBytes))
+
+        assertContentEquals(publicKeyBytes, decodedBytes)
+    }
+}
