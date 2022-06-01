152fd18a2458c3ff46342da6e06967a061a6600f
John Midgley <john.midgley@gmail.com>
2022-05-31 21:52:01 -0700

    Disabled Solr unit tests.

--- opencola-server/src/test/kotlin/opencola/core/search/SolrSearchIndexTest.kt
+++ opencola-server/src/test/kotlin/opencola/core/search/SolrSearchIndexTest.kt
@@ -1,37 +1,33 @@
 package opencola.core.search
 
-import opencola.core.TestApplication
-import opencola.core.model.Id
-import org.junit.Test
-
-class SolrSearchIndexTest {
-    private val authorityId = Id.ofData("authority".toByteArray())
-    private val searchConfig = TestApplication.config.search
-    private val solrSearchIndex = SolrSearchIndex(authorityId, searchConfig.solr!!)
-
-    init {
-        // Make sure we have a fresh index
-        solrSearchIndex.destroy()
-        // This is horrible, but there's no way to know when solr is ready to create the index again other than trying to create the index
-        // TODO: Likely a race condition here between deleting the old index and creating a new one
-        // TODO: Think about overriding index name, so that fresh index can be used. Or just generate new authority for each version of this test.
-        Thread.sleep(1000)
-        solrSearchIndex.create()
-
-    }
-
-    @Test
-    fun testIndexSolr(){
-        testIndex(authorityId, solrSearchIndex)
-    }
-
-    @Test
-    fun testSolrIndexResourceWithMhtml(){
-        testIndexResourceWithMhtml(authorityId, solrSearchIndex)
-    }
-
-    @Test
-    fun testSolrRepeatIndexing(){
-        testRepeatIndexing(authorityId, solrSearchIndex)
-    }
-}
+//class SolrSearchIndexTest {
+//    private val authorityId = Id.ofData("authority".toByteArray())
+//    private val searchConfig = TestApplication.config.search
+//    private val solrSearchIndex = SolrSearchIndex(authorityId, searchConfig.solr!!)
+//
+//    init {
+//        // Make sure we have a fresh index
+//        solrSearchIndex.destroy()
+//        // This is horrible, but there's no way to know when solr is ready to create the index again other than trying to create the index
+//        // TODO: Likely a race condition here between deleting the old index and creating a new one
+//        // TODO: Think about overriding index name, so that fresh index can be used. Or just generate new authority for each version of this test.
+//        Thread.sleep(1000)
+//        solrSearchIndex.create()
+//
+//    }
+//
+//    @Test
+//    fun testIndexSolr(){
+//        testIndex(authorityId, solrSearchIndex)
+//    }
+//
+//    @Test
+//    fun testSolrIndexResourceWithMhtml(){
+//        testIndexResourceWithMhtml(authorityId, solrSearchIndex)
+//    }
+//
+//    @Test
+//    fun testSolrRepeatIndexing(){
+//        testRepeatIndexing(authorityId, solrSearchIndex)
+//    }
+//}
