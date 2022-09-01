8b1dd19bb19d33ce456acb2b20ecf5691b620850
John Midgley <john.midgley@gmail.com>
2022-08-31 18:58:21 -0700

    - Moved authentication from ConnectionHandler.kt to RelayServer.kt - Made server properly manage connections

--- opencola-server/relay/src/main/kotlin/io/opencola/relay/server/ConnectionHandler.kt
+++ opencola-server/relay/src/main/kotlin/io/opencola/relay/server/ConnectionHandler.kt
@@ -1,70 +1,31 @@
 package io.opencola.relay.server
 
-import io.ktor.network.sockets.*
-import io.opencola.core.security.isValidSignature
-import io.opencola.core.security.publicKeyFromBytes
-import io.opencola.relay.readSizedByteArray
-import io.opencola.relay.writeSizedByteArray
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import io.opencola.relay.common.Connection
 import mu.KotlinLogging
-import opencola.core.extensions.toByteArray
 import java.io.Closeable
-import java.util.*
 
 
 // TODO: Multiplex connection?
-class ConnectionHandler(private val socket: Socket) : Closeable {
+class ConnectionHandler(private val connection: Connection) : Closeable {
     private val logger = KotlinLogging.logger("RelayConnection")
-    private val readChannel = socket.openReadChannel()
-    private val writeChannel = socket.openWriteChannel(autoFlush = true)
-    private val connectionMutex = Mutex()
-
-    private suspend fun authenticate() {
-        try {
-            connectionMutex.withLock {
-                val encodedPublicKey = readChannel.readSizedByteArray()
-                val publicKey = publicKeyFromBytes(encodedPublicKey)
-
-                // Send challenge
-                // TODO: Use secure random bit generation
-                val challenge = UUID.randomUUID().toByteArray()
-                writeChannel.writeSizedByteArray(challenge)
-
-                // Read signed challenge
-                val challengeSignature = readChannel.readSizedByteArray()
-
-                val status = if (isValidSignature(publicKey, challenge, challengeSignature)) 0 else -1
-                writeChannel.writeInt(status)
-                if (status != 0)
-                    throw RuntimeException("Client failed to authenticate: ${socket.remoteAddress}")
-            }
-        } catch (e: Exception) {
-            // TODO: Fix anti-pattern of catch all. Coroutines can be canceled, and this stops propagation.
-            // TODO: Log
-            close()
-        }
-    }
 
     private suspend fun handleControlMessage() {
-        when(readChannel.readInt()){
+        when(connection.readInt()){
             // Echo data back
             1 ->  {
                 logger.info { "Handling Echo" }
-                writeChannel.writeSizedByteArray(readChannel.readSizedByteArray())
+                connection.writeSizedByteArray(connection.readSizedByteArray())
             }
-            else -> writeChannel.writeSizedByteArray("ERROR".toByteArray())
+            else -> connection.writeSizedByteArray("ERROR".toByteArray())
         }
     }
 
     suspend fun start() {
         try {
-            authenticate()
-
             while (true) {
                 // val line = readChannel.readUTF8Line()
                 // writeChannel.writeStringUtf8("$line back\n")
-                val recipient = readChannel.readSizedByteArray()
+                val recipient = connection.readSizedByteArray()
 
                 if(recipient.isEmpty()) {
                     handleControlMessage()
@@ -76,6 +37,6 @@ class ConnectionHandler(private val socket: Socket) : Closeable {
     }
 
     override fun close() {
-        socket.close()
+        connection.close()
     }
 }
--- opencola-server/relay/src/main/kotlin/io/opencola/relay/server/RelayServer.kt
+++ opencola-server/relay/src/main/kotlin/io/opencola/relay/server/RelayServer.kt
@@ -2,28 +2,77 @@ package io.opencola.relay.server
 
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
+import io.opencola.core.model.Id
+import io.opencola.core.network.NetworkNode
+import io.opencola.core.security.isValidSignature
+import io.opencola.core.security.publicKeyFromBytes
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import mu.KotlinLogging
+import opencola.core.extensions.toByteArray
+import io.opencola.relay.common.Connection
+import java.security.PublicKey
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 class RelayServer(private val port: Int) {
+    private val logger = KotlinLogging.logger("RelayServer")
     private val selectorManager = ActorSelectorManager(Dispatchers.IO)
     private val serverSocket = aSocket(selectorManager).tcp().bind(port = port)
+    private val connectionHandlers = ConcurrentHashMap<PublicKey, ConnectionHandler>()
     private var started = false
 
     fun isStarted() : Boolean {
         return started
     }
 
+    private suspend fun authenticate(connection: Connection): PublicKey? {
+        try {
+            val encodedPublicKey = connection.readSizedByteArray()
+            val publicKey = publicKeyFromBytes(encodedPublicKey)
+
+            // Send challenge
+            // TODO: Use secure random bit generation
+            val challenge = UUID.randomUUID().toByteArray()
+            connection.writeSizedByteArray(challenge)
+
+            // Read signed challenge
+            val challengeSignature = connection.readSizedByteArray()
+
+            val status = if (isValidSignature(publicKey, challenge, challengeSignature)) 0 else -1
+            connection.writeInt(status)
+            if (status != 0)
+                throw RuntimeException("Challenge signature is not valid")
+
+            return publicKey
+
+        } catch (e: Exception) {
+            // TODO: Fix anti-pattern of catch all. Coroutines can be canceled, and this stops propagation.
+            logger.warn { "Client failed to authenticate: $e" }
+            connection.close()
+        }
+
+        return null
+    }
+
     suspend fun run() = coroutineScope() {
         try {
-            println("Relay Server listening at ${serverSocket.localAddress}")
+            logger.info("Relay Server listening at ${serverSocket.localAddress}")
             started = true
             while (isActive) {
+                logger.info { "Waiting for connections" }
                 val socket = serverSocket.accept()
-                println("Accepted ${socket.remoteAddress}")
-                launch { ConnectionHandler(socket).use { it.start() } }
+                logger.info("Accepted ${socket.remoteAddress}")
+                val connection = Connection(socket)
+                val publicKey = authenticate(connection)
+
+                if(publicKey != null) {
+                    val connectionHandler = ConnectionHandler(connection)
+                    connectionHandlers[publicKey] = connectionHandler
+                    launch { connectionHandler.use { it.start() } }
+                }
             }
         } finally {
             serverSocket.close()
