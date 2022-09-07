670ef8bdd13a847fa16f807b5f35b16650683707
John Midgley <john.midgley@gmail.com>
2022-09-06 18:29:55 -0700

    - Removed TODO

--- opencola-server/relay/src/main/kotlin/io/opencola/relay/common/MessageEnvelope.kt
+++ opencola-server/relay/src/main/kotlin/io/opencola/relay/common/MessageEnvelope.kt
@@ -10,7 +10,6 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.security.PublicKey
 
-// TODO: Should encrypt be done with session key? Make session id encrypted session key?
 class MessageEnvelope(val to: PublicKey, val message: ByteArray) {
     constructor(to: PublicKey, message: Message) : this(to, encrypt(to, Message.encode(message)))
 
