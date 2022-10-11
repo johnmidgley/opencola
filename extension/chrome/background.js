4f2b66bd3f73c2e39e589d6a098dcb1ae893947f
John Midgley <john.midgley@gmail.com>
2022-10-11 09:15:03 -0700

    Chrome Extension Improvements - Got omni box working with oc - Added auth token to options (not used yet) - On install, don't overwrite service url, if it already exists

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,13 +1,27 @@
 let serviceUrl = "http://0.0.0.0:5795";
 
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Loaded openCOLA extension")
-    chrome.storage.sync.set({ serviceUrl });
-    console.log("Default serviceUrl set to " + serviceUrl);
+  chrome.storage.sync.get("serviceUrl", (data) => {
+    if(data.serviceUrl == null) {
+      console.log("Loaded OpenCola extension")
+      chrome.storage.sync.set({ serviceUrl });
+      console.log("Default serviceUrl set to " + serviceUrl);
+    }
+  })
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
+  chrome.storage.sync.get("serviceUrl", (data) => {
+    let serviceUrl = data.serviceUrl
+
     // Encode user input for special characters , / ? : @ & = + $ #
-    const newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
+    const newURL =  serviceUrl + '/#feed?q=' + encodeURIComponent(text);
     chrome.tabs.create({ url: newURL });
+  })
 });
+
+// chrome.webNavigation.onCompleted.addListener(function(details) {
+//     console.log("Nav - oc: " + details.url);
+// },
+//  // {url: [{hostEquals : '0.0.0.0'}]}
+// );
--- extension/chrome/options.js
+++ extension/chrome/options.js
@@ -1,18 +1,28 @@
-function handleOptionsFormSubmit(event){
+function handleOptionsFormSubmit(event) {
   let serviceUrl = document.getElementById("serviceHost").value
   chrome.storage.sync.set({ serviceUrl });
+
+  let authToken = document.getElementById("authToken").value
+  chrome.storage.sync.set({ authToken });
 }
 
 function constructOptions() {
     chrome.storage.sync.get("serviceUrl", (data) => {
-    let serviceUrl = data.serviceUrl
-    let inputServiceUrl = document.getElementById("serviceHost")
-    inputServiceUrl.value = serviceUrl;
+      let serviceUrl = data.serviceUrl
+      let inputServiceUrl = document.getElementById("serviceHost")
+      inputServiceUrl.value = serviceUrl;      
+    });
+
+    chrome.storage.sync.get("authToken", (data) => {
+      let authToken = data.authToken
+      let inputAuthToken = document.getElementById("authToken")
+      inputAuthToken.value = authToken;      
+    });
+
 
     let optionsForm = document.getElementById("optionsForm")
     optionsForm.addEventListener("submit", handleOptionsFormSubmit)
 
-    });
 }
 
 constructOptions();
