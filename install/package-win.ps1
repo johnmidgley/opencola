76aba49d8a6e31eb9889f14cfc57e948878b6229
John Midgley <john.midgley@gmail.com>
2022-12-07 18:14:15 -0800

    Improved Windows installer - Added version, icon, vendor

--- install/package-win.ps1
+++ install/package-win.ps1
@@ -2,7 +2,11 @@
 # Which needs https://wixtoolset.org installed and in path
 # Which needs https://dotnet.microsoft.com/en-us/download/dotnet-framework/thank-you/net35-sp1-web-installer
 
-# Doesn't seem to work
+if ($Args.count -ne 1) {
+	"usage: package-win versions"
+	exit 1
+}
+
 
 echo "Creating Windows Installer"
 # https://docs.oracle.com/en/java/javase/14/docs/specs/man/jpackage.html
@@ -11,9 +15,13 @@ jpackage --input opencola/server/lib/ `
 --main-jar opencola-server-1.0-SNAPSHOT.jar `
 --main-class opencola.server.ApplicationKt `
 --verbose `
---win-console `
 --type msi `
 --java-options '--enable-preview' `
+--icon icons/pull-tab.ico `
+--arguments --desktop `
+--vendor OpenCola `
 --win-shortcut `
+--app-version $Args[0] `
 --win-menu
 # --resource-dir opencola/server/resources/ \
+# --win-console `
