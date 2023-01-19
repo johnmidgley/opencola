97add4e9541f963f9795bdcdb9afc36391387f8f
John Midgley <john.midgley@gmail.com>
2023-01-18 22:43:48 -0800

    Cleanup up relay gradle file

--- opencola-server/relay/build.gradle.kts
+++ opencola-server/relay/build.gradle.kts
@@ -1,12 +1,3 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.7.20"
-}
-
-dependencies {
-    implementation(project(":relay:common"))
-    implementation(project(":relay:server"))
-    implementation(project(":relay:client"))
 }
