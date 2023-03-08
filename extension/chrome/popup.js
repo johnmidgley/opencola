1563d17b9af5e7e97f3671af25528ecdff2480d7
John Midgley <john.midgley@gmail.com>
2023-03-08 14:20:02 -0800

    Enabled personas in extension

--- extension/chrome/popup.js
+++ extension/chrome/popup.js
@@ -12,19 +12,64 @@ function parseCookie(str) {
     }, {});
 }
 
-function checkLoggedIn() {
+function setPersona(id) {
+    chrome.storage.sync.set({"persona-id": id}, () => {
+        console.log("Persona set to: " + id)
+    })
+}
+
+function getPersona(onSuccess, defaultPersonaId = null) {
+    chrome.storage.sync.get("persona-id", (data) => {
+        let personaId = data["persona-id"]
+
+        onSuccess(personaId)
+    })
+}
+
+function setPersonaDefault(id) {
+    // getPersonaId and if not set, set it to the default
+    getPersona((personaId) => {
+        if (personaId == null || personaId == undefined || personaId == "")
+            setPersona(id)
+    })
+}
+
+function removePersona() {
+    chrome.storage.sync.remove("persona-id")
+}
+
+function getPersonas() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    console.log("State change")
     if (this.readyState == 4) {
-        if(this.status != 200) {
+        if(this.status == 200) {
+            let personas = JSON.parse(this.responseText).items
+            let select = document.getElementById("personas")
+            select.onchange = function() { setPersona(this.value) }
+
+            if(personas.length > 0)
+                setPersonaDefault(personas[0].id)
+
+            for(let i = 0; i < personas.length; i++) {
+                let persona = personas[i]
+                let option = document.createElement("option")
+                option.value = persona.id
+                option.text = persona.name
+                select.add(option)
+            }
+
+            getPersona((personaId) => {
+                if(personaId != undefined)
+                select.value = personaId
+            })
+        } else {
           alert("Unable to authenticate extension.\n A login page will be opened.")
           window.close()
           window.open(baseServiceUrl + '/login', "_blank")
         }
     }
   };
-  xhttp.open("GET",  baseServiceUrl + '/isLoggedIn', true);
+  xhttp.open("GET",  baseServiceUrl + '/personas', true);
   xhttp.send();
 }
 
@@ -39,7 +84,7 @@ chrome.storage.sync.get("serviceUrl", (data) => {
           alert("Can't connect to server:\n" + baseServiceUrl + "\nPlease start it and try again.")
           window.close()
         } else {
-          checkLoggedIn()
+            getPersonas()
           setStatus("green")
         }
     }
@@ -63,7 +108,6 @@ function setStatus(status) {
     }
 }
 
-
 async function sendAction(tab, action, value) {
     setStatus("yellow")
 
@@ -76,28 +120,27 @@ async function sendAction(tab, action, value) {
                 formData.append("action", action)
                 formData.append("value", value)
                 formData.append("mhtml", mhtmlData)
-                xhr.open("POST", baseServiceUrl + "/action")
-                xhr.setRequestHeader("authToken", authToken)
-                xhr.onreadystatechange = function () {
-                    // In local files, status is 0 upon success in Mozilla Firefox
-                    if(xhr.readyState === XMLHttpRequest.DONE) {
-                        let status = xhr.status;
-                        if (status >= 200 && status < 400) {
-                            // The request has been completed successfully
-                            setStatus("green")
-                        } else {
-                            setStatus("red")
 
-                            if(status == 401)
-                              login()
+                getPersona((personaId) => {
+                    xhr.open("POST", baseServiceUrl + "/action?personaId=" + personaId)
+                    xhr.setRequestHeader("authToken", authToken)
+                    xhr.onreadystatechange = function () {
+                        // In local files, status is 0 upon success in Mozilla Firefox
+                        if (xhr.readyState === XMLHttpRequest.DONE) {
+                            let status = xhr.status;
+                            if (status >= 200 && status < 400) {
+                                // The request has been completed successfully
+                                setStatus("green")
+                            } else {
+                                setStatus("red")
+
+                                if (status == 401)
+                                    login()
+                            }
                         }
-                    } else {
-                      console.log("Status: " + xhr.status)
-                    }
-                  
-
-                };
-                xhr.send(formData);
+                    };
+                    xhr.send(formData);
+                })
             }
         )
     } catch (error) {
@@ -106,19 +149,16 @@ async function sendAction(tab, action, value) {
     }
 }
 
-
 document.getElementById("save").addEventListener("click", async () => {
     // TODO: For some reason, can't move this call inside send action.
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
   await sendAction(tab, "save", true)
 });
 
-
 document.getElementById("like").addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
   await sendAction(tab, "like", true)
 });
-
 
 document.getElementById("trust").addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
