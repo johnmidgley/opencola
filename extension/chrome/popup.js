8629b2da2d5511e33ca8792dc3df7bf6f7857fec
John Midgley <john.midgley@gmail.com>
2023-03-23 15:38:14 -0700

    Made extension detect when server was started, but the extension was not authenticated (which can happen if you start the server with a different profile / browser). This was failing with no personas showing up.

--- extension/chrome/popup.js
+++ extension/chrome/popup.js
@@ -13,74 +13,76 @@ function parseCookie(str) {
 }
 
 function setPersona(id) {
-    chrome.storage.sync.set({"persona-id": id}, () => {})
+  chrome.storage.sync.set({ "persona-id": id }, () => { })
 }
 
 function getPersona(onSuccess, defaultPersonaId = null) {
-    chrome.storage.sync.get("persona-id", (data) => {
-        onSuccess(data["persona-id"])
-    })
+  chrome.storage.sync.get("persona-id", (data) => {
+    onSuccess(data["persona-id"])
+  })
 }
 
 function setPersonaDefault(personas) {
-    // getPersonaId and if not set, set it to the default
-    getPersona((personaId) => {
-        if(personas.length > 0 && personas.find(p => p.id == personaId) == undefined)
-            setPersona(personas[0].id)
-    })
+  // getPersonaId and if not set, set it to the default
+  getPersona((personaId) => {
+    if (personas.length > 0 && personas.find(p => p.id == personaId) == undefined)
+      setPersona(personas[0].id)
+  })
 }
 
 function removePersona() {
-    chrome.storage.sync.remove("persona-id")
+  chrome.storage.sync.remove("persona-id")
 }
 
 function getPersonas() {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
-        if(this.status == 200) {
-            let personas = JSON.parse(this.responseText).items
-            let select = document.getElementById("personas")
-            select.onchange = function() { setPersona(this.value) }
-            setPersonaDefault(personas)
+      // The check for "{" is a hack to detect when the personas request was redirected to the login page.
+      // There does not seem to be a nice way to do this using XMLHttpRequest.
+      if (this.status == 200 && this.responseText.startsWith("{")) {
+        let personas = JSON.parse(this.responseText).items
+        let select = document.getElementById("personas")
+        select.onchange = function () { setPersona(this.value) }
+        setPersonaDefault(personas)
 
-            for(let i = 0; i < personas.length; i++) {
-                let persona = personas[i]
-                let option = document.createElement("option")
-                option.value = persona.id
-                option.text = persona.name
-                select.add(option)
-            }
-
-            getPersona((personaId) => {
-                if(personaId != undefined && personas.find(p => p.id == personaId) != undefined)
-                    select.value = personaId
-            })
-        } else {
-          alert("Unable to authenticate extension.\n A login page will be opened.")
-          window.close()
-          window.open(baseServiceUrl + '/login', "_blank")
+        for (let i = 0; i < personas.length; i++) {
+          let persona = personas[i]
+          let option = document.createElement("option")
+          option.value = persona.id
+          option.text = persona.name
+          select.add(option)
         }
+
+        getPersona((personaId) => {
+          if (personaId != undefined && personas.find(p => p.id == personaId) != undefined)
+            select.value = personaId
+        })
+      } else {
+        alert("Unable to authenticate extension.\n A login page will be opened.")
+        window.close()
+        window.open(baseServiceUrl + '/login', "_blank")
+      }
     }
   };
-  xhttp.open("GET",  baseServiceUrl + '/personas', true);
+  xhttp.open("GET", baseServiceUrl + '/personas', true);
   xhttp.send();
 }
 
 chrome.storage.sync.get("serviceUrl", (data) => {
   let serviceUrl = data.serviceUrl
   baseServiceUrl = serviceUrl
-  
+
   let xhttp = new XMLHttpRequest()
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
-        if(this.status == 0) {
-          alert("Can't connect to server:\n" + baseServiceUrl + "\nPlease start it and try again.")
-          window.close()
-        } else {
-            getPersonas()
-          setStatus("green")
-        }
+      if (this.status == 0) {
+        alert("Can't connect to server:\n" + baseServiceUrl + "\nPlease start it and try again.")
+        window.close()
+      } else {
+        getPersonas()
+        setStatus("green")
+      }
     }
   };
 
@@ -90,61 +92,61 @@ chrome.storage.sync.get("serviceUrl", (data) => {
 
 
 function setStatus(status) {
-    switch(status){
-        case "red":
-        case "yellow":
-        case "green":
-            statusImg.src ="images/" + status + "-light-icon.svg";
-            return true
-        default:
-            console.log("Invalid status: " + status)
-            return false
-    }
+  switch (status) {
+    case "red":
+    case "yellow":
+    case "green":
+      statusImg.src = "images/" + status + "-light-icon.svg";
+      return true
+    default:
+      console.log("Invalid status: " + status)
+      return false
+  }
 }
 
 async function sendAction(tab, action, value) {
-    setStatus("yellow")
+  setStatus("yellow")
 
-    try {
-        // Save to HTML example from https://stackoverflow.com/questions/32194397/why-isnt-chrome-pagecapture-saveasmhtml-working-in-my-google-chrome-extension
-        chrome.pageCapture.saveAsMHTML(
-            {tabId: tab.id}, function (mhtmlData) {
-                // Good resource: https://javascript.info/blob
-                let xhr = new XMLHttpRequest(), formData = new FormData()
-                formData.append("action", action)
-                formData.append("value", value)
-                formData.append("mhtml", mhtmlData)
+  try {
+    // Save to HTML example from https://stackoverflow.com/questions/32194397/why-isnt-chrome-pagecapture-saveasmhtml-working-in-my-google-chrome-extension
+    chrome.pageCapture.saveAsMHTML(
+      { tabId: tab.id }, function (mhtmlData) {
+        // Good resource: https://javascript.info/blob
+        let xhr = new XMLHttpRequest(), formData = new FormData()
+        formData.append("action", action)
+        formData.append("value", value)
+        formData.append("mhtml", mhtmlData)
 
-                getPersona((personaId) => {
-                    xhr.open("POST", baseServiceUrl + "/action?personaId=" + personaId)
-                    xhr.setRequestHeader("authToken", authToken)
-                    xhr.onreadystatechange = function () {
-                        // In local files, status is 0 upon success in Mozilla Firefox
-                        if (xhr.readyState === XMLHttpRequest.DONE) {
-                            let status = xhr.status;
-                            if (status >= 200 && status < 400) {
-                                // The request has been completed successfully
-                                setStatus("green")
-                            } else {
-                                setStatus("red")
+        getPersona((personaId) => {
+          xhr.open("POST", baseServiceUrl + "/action?personaId=" + personaId)
+          xhr.setRequestHeader("authToken", authToken)
+          xhr.onreadystatechange = function () {
+            // In local files, status is 0 upon success in Mozilla Firefox
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+              let status = xhr.status;
+              if (status >= 200 && status < 400) {
+                // The request has been completed successfully
+                setStatus("green")
+              } else {
+                setStatus("red")
 
-                                if (status == 401)
-                                    login()
-                            }
-                        }
-                    };
-                    xhr.send(formData);
-                })
+                if (status == 401)
+                  login()
+              }
             }
-        )
-    } catch (error) {
-        alert(error)
-        console.log(error)
-    }
+          };
+          xhr.send(formData);
+        })
+      }
+    )
+  } catch (error) {
+    alert(error)
+    console.log(error)
+  }
 }
 
 document.getElementById("save").addEventListener("click", async () => {
-    // TODO: For some reason, can't move this call inside send action.
+  // TODO: For some reason, can't move this call inside send action.
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
   await sendAction(tab, "save", true)
 });
