9be9244829753bd60dce14ad6b308f8b1ec328a6
John Midgley <john.midgley@gmail.com>
2023-04-04 22:35:05 -0700

    Cleaned up attachment control

--- web-ui/src/opencola/web_ui/view/attachments.cljs
+++ web-ui/src/opencola/web_ui/view/attachments.cljs
@@ -4,30 +4,36 @@
    [opencola.web-ui.time :refer [format-time]]
    [opencola.web-ui.ajax :as ajax]))
 
+(defn selected-files-table [file-list!]
+  (fn []
+    (if (some? @file-list!)
+      [:ul (map-indexed (fn [i file]
+                          (js/console.log file)
+                          [:li {:key i} (.-name file)])
+                        @file-list!)])))
+
 (defn select-files-control [file-list!]
   (fn []
     (let [input-id (str (random-uuid))]
-      [:div
+      [:span
        [:input {:type "file"
                 :id input-id
                 :multiple true
                 :style {:display "none"}
                 :on-change #(reset! file-list! (.. % -target -files))}]
-       [:button {:on-click #(.click (js/document.getElementById input-id))} "Select Files"]
-       [:ul (map-indexed (fn [i file]
-                            (js/console.log file)
-                           [:li {:key i} (.-name file)])
-                         @file-list!)]])))
+       [:button {:on-click #(.click (js/document.getElementById input-id))} "Select Files"]])))
 
 (defn attachment-control [persona-id! feed! entity-id expanded?!]
   (if @expanded?!
-  (let [file-list! (atom [])]
-    [:div.attachment-control
-     [:div.attachment-control-header "Attachments:"]
-     [select-files-control file-list!]
-     [:div.attachment-control-footer
-      [:button {:on-click (fn [] (ajax/upload-files (str "/upload?personaId=" @persona-id!) @file-list!))} "Save"]
-      [:button {:on-click (fn [] (swap! expanded?! #(not %)))} "Cancel"]]])))
+    (let [file-list! (atom [])]
+      [:div.attachment-control
+       [:div.attachment-control-header "Attachments:"]
+       [selected-files-table file-list!]
+       [select-files-control file-list!]
+       " "
+       [:button {:on-click (fn [] (ajax/upload-files (str "/upload?personaId=" @persona-id!) @file-list!))} "Attach"]
+       " "
+       [:button {:on-click (fn [] (swap! expanded?! #(not %)))} "Cancel"]])))
 
 (defn item-attachment [action]
   (let [{authority-name :authorityName
