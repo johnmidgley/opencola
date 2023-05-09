f815697edfb152552176db2a8f7770c85ee350e3
John Midgley <john.midgley@gmail.com>
2023-05-08 22:42:16 -0700

    Cleaned up MessageDigest.kt

--- opencola-server/core/security/src/main/kotlin/io/opencola/security/MessageDigest.kt
+++ opencola-server/core/security/src/main/kotlin/io/opencola/security/MessageDigest.kt
@@ -2,16 +2,13 @@ package io.opencola.security
 
 import java.security.MessageDigest
 
-// TODO: Just change to hash, and configure algorithm
 fun sha256(input: ByteArray) : ByteArray{
     // Can use update() method to add in length
     return MessageDigest
-        .getInstance("SHA-256") // TODO: SHA 3?
+        .getInstance("SHA-256")
         .digest(input)
 }
 
 fun sha256(input: String): ByteArray {
     return sha256(input.toByteArray())
 }
-
-fun getMd5Digest(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(Charsets.UTF_8))
