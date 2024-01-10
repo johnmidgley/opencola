d85765169b18c12bb8845337213fdc79e4dad6ff
John Midgley <john.midgley@gmail.com>
2024-01-09 17:53:07 -0800

    Output version on startup

---
+++ opencola-server/core/application/src/main/kotlin/io/opencola/application/Version.kt
@@ -0,0 +1,4 @@
+package io.opencola.application
+
+// This is set by set-version in opencola-server. DO NOT SET MANUALLY (it should be in sync the version the gradle file)
+const val OC_VERSION = "1.3.4"
