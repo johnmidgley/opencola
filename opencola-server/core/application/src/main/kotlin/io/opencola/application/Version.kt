7331fb6b0ee60c7744f4f14b37b7ddcf2f51b12e
John Midgley <john.midgley@gmail.com>
2024-03-26 15:52:47 -0700

    Updated to version 1.4.0

--- opencola-server/core/application/src/main/kotlin/io/opencola/application/Version.kt
+++ opencola-server/core/application/src/main/kotlin/io/opencola/application/Version.kt
@@ -18,4 +18,4 @@
 package io.opencola.application
 
 // This is set by set-version in opencola-server. DO NOT SET MANUALLY (it should be in sync the version the gradle file)
-const val OC_VERSION = "1.3.4"
+const val OC_VERSION = "1.4.0"
